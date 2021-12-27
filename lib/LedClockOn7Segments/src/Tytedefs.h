560f1e11b35abce87e119dea2501d31856893f56
Xander Ch <xandrmail@mail.ru>
2021-12-27 16:13:39 +0600

    поменял сохранялку настроек

--- lib/LedClockOn7Segments/src/Tytedefs.h
+++ lib/LedClockOn7Segments/src/Tytedefs.h
@@ -1,32 +1,68 @@
 #ifndef Typedefs_h
 #define Typedefs_h
 #include <LibEnums.h>
-/*
-struct DisplaySettings{
-    byte brightness = 20;
-    CHSV subZeroColor = CHSV(HUE_BLUE, 255, 255);       // цвет отрисовки температуры 
-    CHSV plusZeroColor = CHSV(HUE_ORANGE, 255, 255);    // цвет отрисовки температуры
-    CHSV clockColor = CHSV(HUE_GREEN, 255, 255);        // цвет отрисовки времени 
-    CHSV custormColor = CHSV(HUE_GREEN, 255, 255);
-};*/
-
+#include <EEPROM.h>
 struct DisplaySettings{
     int8_t brightness = 60;
     int8_t subZeroColor = 0;       // цвет отрисовки температуры 
     int8_t plusZeroColor = 20;    // цвет отрисовки температуры
     int8_t clockColor = 150;        // цвет отрисовки времени 
-    int8_t custormColor = 43;
+    int8_t iconsColor = 43;
+    int8_t periodTemp = 60;
+    int8_t durationTemp = 15;
     DisplaySettings operator=(DisplaySettings const &other){
         brightness = other.brightness;
         subZeroColor = other.subZeroColor;
         plusZeroColor = other.plusZeroColor;
         clockColor = other.clockColor;
-        custormColor = other.custormColor;
+        iconsColor = other.iconsColor;
+        periodTemp = other.periodTemp;
         return *this;
     }
     int8_t getBrightness(){
         if(brightness < 60) brightness = 60;
         return brightness;
+    }
+    void save(){
+        EEPROMWriteInt(0,27575);
+        EEPROMWriteInt(2,clockColor);
+        EEPROM.write(4,subZeroColor);
+        EEPROM.write(5, iconsColor);
+        EEPROM.write(6,plusZeroColor);
+        EEPROM.write(7,durationTemp);
+        EEPROM.write(8,brightness);
+        EEPROM.write(9,periodTemp);
+
+    }
+    void load(){
+        if(EEPROMReadInt(0) == 27575){
+            this->clockColor =  EEPROMReadInt(2);
+            this->subZeroColor = EEPROM.read(4);
+            this->iconsColor = EEPROM.read(5);
+            this->plusZeroColor = EEPROM.read(6);
+            this->durationTemp = EEPROM.read(7);
+            this->brightness = EEPROM.read(8);
+            this->periodTemp = EEPROM.read(9);
+        }else{
+            save();
+        }
+        
+    }
+private:
+    void EEPROMWriteInt(int p_address, int p_value){
+        byte lowByte = ((p_value >> 0) & 0xFF);
+        byte highByte = ((p_value >> 8) & 0xFF);
+
+        EEPROM.write(p_address, lowByte);
+        EEPROM.write(p_address + 1, highByte);
+        }
+
+//This function will read a 2 byte integer from the eeprom at the specified address and address + 1
+    int EEPROMReadInt(int p_address){
+        byte lowByte = EEPROM.read(p_address);
+        byte highByte = EEPROM.read(p_address + 1);
+
+        return ((lowByte << 0) & 0xFF) + ((highByte << 8) & 0xFF00);
     }
 };
 
